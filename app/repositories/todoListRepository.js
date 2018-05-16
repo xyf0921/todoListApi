a2118b16342a45d3de077cda9f6776512cdad80e
xyf0921 <974708644@qq.com>
2018-05-16 17:29:29 +0800

    second commit

--- app/repositories/todoListRepository.js
+++ app/repositories/todoListRepository.js
@@ -12,22 +12,40 @@ class TodoListRepository {
 
   listAllTodos() {
     //实现查看所有todos的方法
+    return this.todoList;
   }
 
   findTodoBy(id) {
     //实现通过id查看具体todo的方法
+    for(var i=0 ; i<this.todoList.length ; i++)
+        if(this.todoList[i].id === id)
+            return this.todoList[i];
   }
 
   createTodo(todoBody) {
     //实现创建新todo纪录的方法
+    this.todoList.push(todoBody);
   }
 
   updateTodo(id, update) {
     //实现通过id和一个更新对象来更新todo纪录的方法
+    for(var i=0 ; i<this.todoList.length ; i++)
+        if(this.todoList[i].id === id){
+            this.todoList[i].name = update.name;
+            this.todoList[i].description = update.description;
+            this.todoList[i].isFinished = update.isFinished;
+            this.todoList[i].createdAt = update.createdAt;
+            break;
+        }
+            
   }
 
   deleteTodoBy(id) {
     //实现通过id来删除todo纪录的方法
+    for(var i=0 ; i<this.todoList.length ; i++)
+        if(this.todoList[i].id === id)
+            this.todoList.splice(i,1);
+            break;
   }
 }
 
